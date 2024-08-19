f1d90b2c82f0e871a05b3ac6d6d6de5c9d05a9b2
yegenchun <yegenchun@tinman.cn>
2024-08-19 22:40:00 +0800

    增加SO文件作为字典存储处理前后的食材，完善切菜的功能

--- Assets/Scripts/Counter/CuttingCounter.cs
+++ Assets/Scripts/Counter/CuttingCounter.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class CuttingCounter : BaseCounter
 {
-    [SerializeField] private KitchenObjectSO cutKitchenObjectSO;
+    // 相当于字典，存储所有处理前和处理后的食材数据
+    [SerializeField] private CuttingRecipeSO[] cuttingRecipeSOArray;
     
     /// <summary>
     /// 与柜台交互，拿取
@@ -14,8 +15,9 @@ public class CuttingCounter : BaseCounter
         if (!this.HasKitchenObject()) {
             // 柜台本身没有物品，就可以放置物品
             if (player.HasKitchenObject()) {
-                // 如果玩家拥有物品，才可放置在柜台上
-                player.GetKitchenObject().SetKitchenObjectParent(this);
+                // 如果玩家拥有的食材可被处理，才可放置在柜台上
+                if(HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO()))
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
             }else {
                 // 如果玩家没有物品，柜台也没有物品，就不能与柜台进行交互
             }
@@ -36,11 +38,35 @@ public class CuttingCounter : BaseCounter
     /// </summary>
     /// <param name="player"></param>
     public override void InteractAlternate(Player player) {
-        if (this.HasKitchenObject()) {
-            // 柜台本身有食材，就可以处理食材
+        
+        if (this.HasKitchenObject() && HasRecipeWithInput(GetKitchenObject().GetKitchenObjectSO())) {
+            // 只有柜台有食材后，并且该食材能够被处理，才能进行处理
+            // 得到处理后的食材数据
+            KitchenObjectSO outputKitchenObject = GetOutputForInput(GetKitchenObject().GetKitchenObjectSO());
             GetKitchenObject().DestroySelf();
             // 生成食材，并且设置其拥有者
-            KitchenObject.SpawnKitchenObject(cutKitchenObjectSO, this);
+            KitchenObject.SpawnKitchenObject(outputKitchenObject, this);
         }
+    }
+
+    // 判断食材能否处理
+    private bool HasRecipeWithInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        foreach (CuttingRecipeSO cuttingRecipeSO in cuttingRecipeSOArray)
+        {
+            if (cuttingRecipeSO.input == inputKitchenObjectSO)
+                return true;
+        }
+        return false;
+    }
+    
+    // 返回处理后的食材
+    private KitchenObjectSO GetOutputForInput(KitchenObjectSO inputKitchenObjectSO) {
+        foreach (CuttingRecipeSO cuttingRecipeSO in cuttingRecipeSOArray)
+        {
+            if (cuttingRecipeSO.input == inputKitchenObjectSO)
+                return cuttingRecipeSO.output;
+        }
+        return null;
     }
 }
